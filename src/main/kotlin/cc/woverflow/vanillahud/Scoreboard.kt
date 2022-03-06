87c51c0215a9383e1a8d15f3af4b1236ae8786c1
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-03-06 09:12:58 +0700

    1.1.1.1.1.1.1.1.1

--- src/main/kotlin/cc/woverflow/vanillahud/Scoreboard.kt
+++ src/main/kotlin/cc/woverflow/vanillahud/Scoreboard.kt
@@ -1,10 +1,9 @@
 package cc.woverflow.vanillahud
 
-import cc.woverflow.onecore.utils.withScale
-import cc.woverflow.onecore.utils.withTranslate
 import cc.woverflow.vanillahud.config.VanillaHUDConfig
 import com.google.common.collect.Iterables
 import gg.essential.universal.ChatColor
+import gg.essential.universal.UGraphics
 import gg.essential.universal.UMinecraft
 import gg.essential.universal.UResolution
 import net.minecraft.client.gui.Gui
@@ -17,6 +16,8 @@ object Scoreboard {
 
     fun renderScoreboard(objective: ScoreObjective) {
         if (VanillaHUDConfig.scoreboard) {
+            UGraphics.GL.pushMatrix()
+
             val scoreboard = objective.scoreboard
             val sortedScores = scoreboard.getSortedScores(objective)
             val list = sortedScores.filter {
@@ -43,71 +44,71 @@ object Scoreboard {
             val bottom = (UResolution.scaledHeight / 2 + textHeight / 3) + VanillaHUDConfig.scoreboardY
             val left = (UResolution.scaledWidth - i - 3) + VanillaHUDConfig.scoreboardX
             val scorePointRight = (UResolution.scaledWidth - 1) + VanillaHUDConfig.scoreboardX
-            withTranslate(
-                (-scorePointRight).toFloat() * (VanillaHUDConfig.scoreboardScale - 1f),
-                (-(bottom - collection.size * UMinecraft.getFontRenderer().FONT_HEIGHT)).toFloat() * (VanillaHUDConfig.scoreboardScale - 1f),
-                0.0F
-            ) {
-                withScale(VanillaHUDConfig.scoreboardScale, VanillaHUDConfig.scoreboardScale, 1.0F) {
-                    if (VanillaHUDConfig.scoreboardBackgroundBorder) {
-                        drawHollowRect(
-                            left - 2 - VanillaHUDConfig.scoreboardBorderWidth,
-                            bottom - (collection.size + 1) * UMinecraft.getFontRenderer().FONT_HEIGHT - VanillaHUDConfig.scoreboardBorderWidth,
-                            scorePointRight,
-                            bottom,
-                            VanillaHUDConfig.scoreboardBorderWidth,
-                            VanillaHUDConfig.scoreboardBorderColor.rgb
-                        )
-                    }
-                    collection.forEachIndexed { index, score ->
-                        val scoreplayerteam = scoreboard.getPlayersTeam(score.playerName)
-                        val text = ScorePlayerTeam.formatPlayerName(scoreplayerteam, score.playerName)
-                        val scorePoint = "${ChatColor.RED}${score.scorePoints}"
-                        val currentTextTop = bottom - (index + 1) * UMinecraft.getFontRenderer().FONT_HEIGHT
-                        if (VanillaHUDConfig.scoreboardBackground) Gui.drawRect(
-                            left - 2,
-                            currentTextTop,
-                            scorePointRight,
-                            currentTextTop + UMinecraft.getFontRenderer().FONT_HEIGHT,
-                            VanillaHUDConfig.scoreboardBackgroundColor.rgb
-                        )
-                        UMinecraft.getFontRenderer().drawString(
-                            text, left.toFloat(), currentTextTop.toFloat(), -1, VanillaHUDConfig.scoreboardTextShadow
-                        )
-                        if (VanillaHUDConfig.scoreboardScorePoints) UMinecraft.getFontRenderer().drawString(
-                            scorePoint,
-                            (scorePointRight - UMinecraft.getFontRenderer().getStringWidth(scorePoint)).toFloat(),
-                            currentTextTop.toFloat(),
-                            -1,
-                            VanillaHUDConfig.scoreboardTextShadow
-                        )
-                        if ((index + 1) == collection.size) {
-                            val topText = objective.displayName
-                            if (VanillaHUDConfig.scoreboardBackground) Gui.drawRect(
-                                left - 2,
-                                currentTextTop - UMinecraft.getFontRenderer().FONT_HEIGHT - 1,
-                                scorePointRight,
-                                currentTextTop - 1,
-                                VanillaHUDConfig.scoreboardTitleBackgroundColor.rgb
-                            )
-                            if (VanillaHUDConfig.scoreboardBackground) Gui.drawRect(
-                                left - 2,
-                                currentTextTop - 1,
-                                scorePointRight,
-                                currentTextTop,
-                                VanillaHUDConfig.scoreboardBackgroundColor.rgb
-                            )
-                            UMinecraft.getFontRenderer().drawString(
-                                topText,
-                                (left + i / 2 - UMinecraft.getFontRenderer().getStringWidth(topText) / 2).toFloat(),
-                                (currentTextTop - UMinecraft.getFontRenderer().FONT_HEIGHT).toFloat(),
-                                -1,
-                                VanillaHUDConfig.scoreboardTextShadow
-                            )
-                        }
-                    }
+            UGraphics.GL.translate(
+                (-scorePointRight).toDouble() * (VanillaHUDConfig.scoreboardScale - 1f),
+                (-(bottom - collection.size * UMinecraft.getFontRenderer().FONT_HEIGHT)).toDouble() * (VanillaHUDConfig.scoreboardScale - 1f),
+                0.0
+            )
+            UGraphics.GL.scale(VanillaHUDConfig.scoreboardScale, VanillaHUDConfig.scoreboardScale, 1.0F)
+            if (VanillaHUDConfig.scoreboardBackgroundBorder) {
+                drawHollowRect(
+                    left - 2 - VanillaHUDConfig.scoreboardBorderWidth,
+                    bottom - (collection.size + 1) * UMinecraft.getFontRenderer().FONT_HEIGHT - VanillaHUDConfig.scoreboardBorderWidth,
+                    scorePointRight,
+                    bottom,
+                    VanillaHUDConfig.scoreboardBorderWidth,
+                    VanillaHUDConfig.scoreboardBorderColor.rgb
+                )
+            }
+            collection.forEachIndexed { index, score ->
+                val scoreplayerteam = scoreboard.getPlayersTeam(score.playerName)
+                val text = ScorePlayerTeam.formatPlayerName(scoreplayerteam, score.playerName)
+                val scorePoint = "${ChatColor.RED}${score.scorePoints}"
+                val currentTextTop = bottom - (index + 1) * UMinecraft.getFontRenderer().FONT_HEIGHT
+                if (VanillaHUDConfig.scoreboardBackground) Gui.drawRect(
+                    left - 2,
+                    currentTextTop,
+                    scorePointRight,
+                    currentTextTop + UMinecraft.getFontRenderer().FONT_HEIGHT,
+                    VanillaHUDConfig.scoreboardBackgroundColor.rgb
+                )
+                UMinecraft.getFontRenderer().drawString(
+                    text, left.toFloat(), currentTextTop.toFloat(), -1, VanillaHUDConfig.scoreboardTextShadow
+                )
+                if (VanillaHUDConfig.scoreboardScorePoints) UMinecraft.getFontRenderer().drawString(
+                    scorePoint,
+                    (scorePointRight - UMinecraft.getFontRenderer().getStringWidth(scorePoint)).toFloat(),
+                    currentTextTop.toFloat(),
+                    -1,
+                    VanillaHUDConfig.scoreboardTextShadow
+                )
+                if ((index + 1) == collection.size) {
+                    val topText = objective.displayName
+                    if (VanillaHUDConfig.scoreboardBackground) Gui.drawRect(
+                        left - 2,
+                        currentTextTop - UMinecraft.getFontRenderer().FONT_HEIGHT - 1,
+                        scorePointRight,
+                        currentTextTop - 1,
+                        VanillaHUDConfig.scoreboardTitleBackgroundColor.rgb
+                    )
+                    if (VanillaHUDConfig.scoreboardBackground) Gui.drawRect(
+                        left - 2,
+                        currentTextTop - 1,
+                        scorePointRight,
+                        currentTextTop,
+                        VanillaHUDConfig.scoreboardBackgroundColor.rgb
+                    )
+                    UMinecraft.getFontRenderer().drawString(
+                        topText,
+                        (left + i / 2 - UMinecraft.getFontRenderer().getStringWidth(topText) / 2).toFloat(),
+                        (currentTextTop - UMinecraft.getFontRenderer().FONT_HEIGHT).toFloat(),
+                        -1,
+                        VanillaHUDConfig.scoreboardTextShadow
+                    )
                 }
             }
+
+            UGraphics.GL.popMatrix()
         }
     }
 
